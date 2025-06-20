95e272dcef7bc03facb0c7da88f08ffa2179af39
eradix <gerald.digamon@sample.com>
2025-06-20 11:56:11 +0800

    [note-hub] created models, factory and implement database seeder

---
+++ database/factories/NoteTagFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class NoteTagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'note_id' => rand(1,10),
+            'tag_id' => rand(1,4)
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $test = request()->all();
     return view('welcome');
 });
